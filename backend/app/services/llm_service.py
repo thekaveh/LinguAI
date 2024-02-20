cbc1ed9f8de599a438d1492fe725c5e165ab56fd
Rameshkumar Govindaraju <rameshkumar@vt.edu>
2024-02-20 00:14:40 -0500

    removed print statements
    removed unnecessary print statements

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -31,24 +31,22 @@ class LLMService:
 
     @staticmethod
     def list_models() -> List[str]:
+        """
+        A static method to list models and return a list of strings.
+        
+        """
         try:
             # Fetch models from OLLAMA_API_ENDPOINT
             models = [f"{model['model']}" for model in Client(host=Config.OLLAMA_API_ENDPOINT).list()['models']]
-            
-            print("Config.OPENAI_API_KEY : ",Config.OPENAI_API_KEY)
             # Check if OPENAI_API_KEY is not empty
             if Config.OPENAI_API_KEY:
                 # If OPENAI_API_KEY is not empty, split the OPENAI_API_MODEL_LIST string
                 openai_models = Config.OPENAI_MODEL_LIST.split()
-                print("openai_models : ",openai_models)
                 # Add the models specified in OPENAI_MODEL_LIST to the list
                 models.extend(openai_models)
-                print("models : ",models)
             else:
                 # If OPENAI_API_KEY is empty, add an empty string to the list
                 models.append("")
-                print("models NO KEY : ",models)
-            
             return models
         except Exception as e:
             raise Exception("Error fetching models") from e
