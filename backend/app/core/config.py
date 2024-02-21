9f5a5f2cd0b9730fb88a01bab61ad6aef0701d4b
Kaveh <kaveh.razavi@gmail.com>
2024-02-20 21:10:08 -0500

    fixes both backend's llm_service.list and frontend's state_service's handling of initalization of model and personal selected values

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,14 @@
 import os
 import json
 
+
 class Config:
     """
     Configuration class for the application.
     """
 
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
-    OPENAI_MODEL_LIST = os.environ.get("OPENAI_MODEL_LIST", "")
+    OPENAI_MODELS = os.environ.get("OPENAI_MODELS", "")
     OLLAMA_OPENAI_API_KEY = "ollama"
     OPENAI_API_ENDPOINT = os.environ.get("OPENAI_API_ENDPOINT", "")
     OLLAMA_API_ENDPOINT = os.environ.get("OLLAMA_API_ENDPOINT", "")
--- frontend/components/settings.py
+++ frontend/components/settings.py
@@ -5,44 +5,47 @@ from services.llm_service import LLMService
 from services.state_service import StateService
 from services.persona_service import PersonaService
 
+
 def render():
     st.title("Settings")
-    
+
     state_service = StateService.instance()
 
     with st.expander("Chat", expanded=True):
         st.markdown("<hr>", unsafe_allow_html=True)
-        
+
         models = asyncio.run(LLMService.list())
         personas = asyncio.run(PersonaService.list())
-        
+
         new_model = st.selectbox(
-			label="Model:"
-			, options=models
-			, key="chat.model"
-			, index=models.index(state_service.model)
-		)
-        
-        st.markdown("<hr>", unsafe_allow_html=True)
-        
-        new_temperature = st.slider(
-            step=0.1
-            , min_value=0.0
-            , max_value=1.0
-            , label="Temperature:"
-            , key="chat.temperature"
-            , value=state_service.temperature
+            label="Model:",
+            key="chat.model",
+            disabled=not models,
+            options=models if models else ["No models available"],
+            index=models.index(state_service.model) if models else 0,
         )
-        
+
         st.markdown("<hr>", unsafe_allow_html=True)
-        
+
+        new_temperature = st.slider(
+            step=0.1,
+            min_value=0.0,
+            max_value=1.0,
+            label="Temperature:",
+            key="chat.temperature",
+            value=state_service.temperature,
+        )
+
+        st.markdown("<hr>", unsafe_allow_html=True)
+
         new_persona = st.selectbox(
-			label="Persona:"
-			, options=personas
-			, key="chat.persona"
-			, index=personas.index(state_service.persona)
-		)
-        
+            label="Persona:",
+            key="chat.persona",
+            disabled=not personas,
+            options=personas if personas else ["No personas available"],
+            index=personas.index(state_service.persona) if personas else 0,
+        )
+
         state_service.model = new_model
         state_service.persona = new_persona
         state_service.temperature = new_temperature
