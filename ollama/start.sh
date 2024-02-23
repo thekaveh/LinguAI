a5dcb63346e4c086fd3c24689fd129d18e590c47
Kaveh <kaveh.razavi@gmail.com>
2024-02-23 16:31:08 -0500

    adds the rewritten start.sh with LF

---
+++ ollama/start.sh
@@ -0,0 +1,33 @@
+ollama serve &
+
+check_ollama_running() {
+	if curl -s http://localhost:11434 | grep -q "Ollama is running"; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+echo "Waiting for Ollama service to be ready..."
+until check_ollama_running; do
+	sleep 1
+	echo -n "."
+done
+echo "Ollama service is ready."
+
+IFS=' ' read -r -a image_array <<< "${OLLAMA_MODELS}"
+
+echo "Models to be pulled: ${image_array[*]}"
+
+for image in "${image_array[@]}"; do
+	echo "Pulling image: $image"
+	if ollama pull "$image"; then
+		echo "$image successfully pulled."
+	else
+		echo "Failed to pull $image."
+		exit 1
+	fi
+done
+
+# Keep the container running by waiting on all background processes
+wait
