df04620881ac7a4b8a45e8d1251e7cb784d1cf45
Kaveh Razavi <kaveh.razavi@gmail.com>
2024-03-11 23:09:40 -0400

    updates persona's code formatting

--- backend/app/api/v1/endpoints/persona.py
+++ backend/app/api/v1/endpoints/persona.py
@@ -8,8 +8,10 @@ from app.services.dependency.db_service import get_db_session
 
 router = APIRouter()
 
+
 def _get_service(db_session: Session = Depends(get_db_session)) -> PersonaService:
     return PersonaService(db_session=db_session)
+
 
 @log_decorator
 @router.get("/personas/")
@@ -19,42 +21,49 @@ def get_all(service: PersonaService = Depends(_get_service)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @log_decorator
 @router.get("/personas/by_id/{id}", response_model=None)
 def get_by_id(id: int, service: PersonaService = Depends(_get_service)):
-	persona = service.get_by_id(id)
-	
-	if persona is None:
-		raise HTTPException(status_code=404, detail="Persona not found!")
-	
-	return persona
-    
+    persona = service.get_by_id(id)
+
+    if persona is None:
+        raise HTTPException(status_code=404, detail="Persona not found!")
+
+    return persona
+
+
 @log_decorator
-@router.get("/personas/by_name/{id}", response_model=None)
+@router.get("/personas/by_name/{name}", response_model=None)
 def get_by_name(name: str, service: PersonaService = Depends(_get_service)):
-	persona = service.get_by_name(name)
-	
-	if persona is None:
-		raise HTTPException(status_code=404, detail="Persona not found!")
-	
-	return persona
+    persona = service.get_by_name(name)
+
+    if persona is None:
+        raise HTTPException(status_code=404, detail="Persona not found!")
+
+    return persona
+
 
 @router.post("/personas/")
 def create(persona: Persona, service: PersonaService = Depends(_get_service)):
     try:
-    	return service.create(persona)
+        return service.create(persona)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @log_decorator
 @router.put("/personas/{id}")
-def update_persona(id: int, persona: Persona, service: PersonaService = Depends(_get_service)):
+def update_persona(
+    id: int, persona: Persona, service: PersonaService = Depends(_get_service)
+):
     updated = service.update(id, persona)
 
     if updated is None:
         raise HTTPException(status_code=404, detail="Persona not found!")
 
     return updated
+
 
 @log_decorator
 @router.delete("/personas/{id}", response_model=None)
