dceb38439c522483a191edda229556eeb91333a1
Kaveh Razavi <kaveh.razavi@gmail.com>
2024-04-06 00:17:08 -0400

    updates docker compose, backend/main.py, the db snapshot, and llm models

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from sqlmodel import Session
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
 
 from app.core.config import Config
-from app.services.llm_service import LLMService
 from app.api.v1.router import router as v1_router
 from app.services.dependency.db_service import init_db
 from app.utils.logger_config import setup_global_logging
-from app.services.dependency.db_service import db_engine
 
 # Setup global logging with a specific logger name
 setup_global_logging(
@@ -28,23 +23,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(v1_router)
-background_scheduler = BackgroundScheduler()
-
-
-@app.on_event("startup")
-async def on_startup():
-    init_db()
-
-    llm_service = LLMService(db_session=Session(db_engine))
-
-    background_scheduler.add_job(
-        llm_service.init_ollama, "date", run_date=datetime.now()
-    )
-
-    background_scheduler.start()
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    print("FastApi: shutdown")
-    background_scheduler.shutdown()
